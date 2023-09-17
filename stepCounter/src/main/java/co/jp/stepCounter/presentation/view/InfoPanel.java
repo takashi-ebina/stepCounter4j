95d2d9a3bc0770c915319b44acd44bdb5cae55b8
海老名　能司 <bluesky.bluesnow.2015@gmail.com>
2023-09-18 01:18:51 +0900

    テキストブロック記法に修正

--- stepCounter/src/main/java/co/jp/stepCounter/presentation/view/InfoPanel.java
+++ stepCounter/src/main/java/co/jp/stepCounter/presentation/view/InfoPanel.java
@@ -26,17 +26,18 @@ public class InfoPanel extends JPanel {
 	 */
 	public void init() {
 		final String strHtml = 
-				"<html><h1><font Size=6>stepcounterについて</font></h1>"
-				+ "<p>プログラムのステップ数を集計するツールです。</p>"
-				+ "<br>"
-				+ "<p>対応プログラムファイル：Java、Cs、sql<br>"
-				+ "作成者：takashi.ebina<br>"
-				+ "GitHubリポジトリ：https://github.com/takashi-ebina/stepCounterforJava/</p>"
-				+ "<br>"
-				+ "<h2><font Size=5>変更履歴</font></h2>"
-				+ "<h3><font Size=4>1.0.0</font></h3>"
-				+ "新規リリース"
-				;
+				"""
+				<html><h1><font Size=6>stepcounterについて</font></h1>
+				<p>プログラムのステップ数を集計するツールです。</p>
+				<br>
+				<p>対応プログラムファイル：Java、Cs、sql<br>
+				作成者：takashi.ebina<br>
+				GitHubリポジトリ：https://github.com/takashi-ebina/stepCounterforJava/</p>
+				<br>
+				<h2><font Size=5>変更履歴</font></h2>
+				<h3><font Size=4>1.0.0</font></h3>
+				新規リリース
+				""";
 		this.add(new JLabel(strHtml));
 		this.setOpaque(false);
 		this.setPreferredSize(new Dimension(750, 440));
